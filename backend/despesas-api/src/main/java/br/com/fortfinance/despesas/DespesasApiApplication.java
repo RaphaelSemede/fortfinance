76fd8f47540b9cdd2b7f08a3a30c4b542122d23c
Raphael <raphasemede@gmail.com>
2025-04-23 20:33:22 -0300

    Sets up Maven project structure for despesas-api
    Creates initial Maven configuration files and project structure.
    
    Includes necessary dependencies for Spring Boot application and sets up main application class and test cases.
    
    Prepares project for development and testing.

---
+++ backend/despesas-api/src/main/java/br/com/fortfinance/despesas/DespesasApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fortfinance.despesas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DespesasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DespesasApiApplication.class, args);
+	}
+
+}
---
+++ backend/despesas-api/src/test/java/br/com/fortfinance/despesas/DespesasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fortfinance.despesas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DespesasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
